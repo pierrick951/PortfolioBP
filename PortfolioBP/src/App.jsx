595602ad5b3b0bb7823e874b75423d4f3336bdad
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-09 18:30:28 -0400

    creation du squelette et ajouter de react router

--- PortfolioBP/src/App.jsx
+++ PortfolioBP/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Skill from './components/Skill'
+import NotFound from './components/NotFound'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div>
-      <p className='bg-red-300 p-5 flex justify-center'>salut</p>
-    </div>
+    <>
+      <BrowserRouter>
+       <Navbar>
+       <Routes>
+        <Route  path="/" element={<Home />}/>
+        <Route  path="/About" element={<About/>}/>
+        <Route  path="/skill" element={<Skill/>}/>
+        <Route  path="/contact" element={<Contact/>}/>
+        <Route  path="*" element={<NotFound/>}/>
+        </Routes>
+       </Navbar>     
+      </BrowserRouter>
+    </>
   )
 }
 
