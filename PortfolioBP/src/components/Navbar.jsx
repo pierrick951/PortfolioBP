7803c73fd2009789cef64898e0703101fed6d3e6
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-09 20:33:02 -0400

    modification de la couleur des links en blanc

--- PortfolioBP/src/components/Navbar.jsx
+++ PortfolioBP/src/components/Navbar.jsx
@@ -6,22 +6,22 @@ export default function Navbar() {
 
    <nav className="bg-gradient-to-r from-indigo-700 to-purple-900 w-full h-24 flex justify-end items-center">
      <NavLink to="/"
-     className={   ({isActive}) => `${isActive && "border-b-2 border-white p-1"}`}
+     className={   ({isActive}) => `${isActive && "border-b-2 border-white p-1"}  text-white`}
      >
       Home
      </NavLink>
      <NavLink to="/about"
-     className={   ({isActive}) => `${isActive && "border-b-2 border-white p-1"}`}
+     className={   ({isActive}) => `${isActive && "border-b-2 border-white p-1"}  text-white`}
      >
       About
      </NavLink>
      <NavLink to="/skill"
-     className={ ({isActive}) => `${isActive && "border-b-2 border-white p-1"}`}
+     className={ ({isActive}) => `${isActive && "border-b-2 border-white p-1"}  text-white`}
      >
     Skill
      </NavLink>
      <NavLink to="/contact"
-     className={   ({isActive}) => `${isActive && "border-b-2 border-white p-1"}`}
+     className={   ({isActive}) => `${isActive && "border-b-2 border-white p-1"} text-white`}
      >
     Contact
      </NavLink>
