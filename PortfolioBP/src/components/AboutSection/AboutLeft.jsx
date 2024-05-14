a340d7b24c54a57c0dc214ee61aa0a2e964caf7c
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-14 19:45:06 -0400

    modification de la section about

--- PortfolioBP/src/components/AboutSection/AboutLeft.jsx
+++ PortfolioBP/src/components/AboutSection/AboutLeft.jsx
@@ -1,5 +1,6 @@
 import './AboutSection.css'
 
+
 export default function AboutLeft() {
   return (
     <div className='LeftboxHeading'>
@@ -7,7 +8,9 @@ export default function AboutLeft() {
       <h1> A Self Taught Front-end Developper based in Montreal, Canada📍</h1>
       <div className='LeftboxPara'>
         <p>Junior Front-End Developer with expertise in HTML, CSS, JavaScript, React, Tailwind, and SCSS. Specialize in crafting dynamic, responsive websites for optimal user experience. Skilled in clean, efficient code and cutting-edge development tools. Collaborative team player adept at producing web applications efficiently.</p>
+
       </div>
+      <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f680/512.gif" alt="🚀" width="32" height="32"/>
     </div>
   )
 }
--- PortfolioBP/src/components/AboutSection/AboutRigth.jsx
+++ PortfolioBP/src/components/AboutSection/AboutRigth.jsx
@@ -1,11 +1,12 @@
 import computer from '../../assets/computer.png'
+import japan from '../../assets/japan.jpg'
 
 import './AboutSection.css'
 
 export default function AboutRigth() {
     return (
         <div className="ContainerRigth">
-            <img src={computer} alt="image of computer" />
+            <img src={japan} alt="image of computer" />
         </div>
     )
 }
