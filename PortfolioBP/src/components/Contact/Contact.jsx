815bc2bb75bc3d5b31f764c147c1c8adfaf18d8b
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-15 22:44:29 -0400

    update

--- PortfolioBP/src/components/Contact/Contact.jsx
+++ PortfolioBP/src/components/Contact/Contact.jsx
@@ -1,20 +1,27 @@
 import './Contact.css'
-
 import mail from '../../assets/mail.svg'
+import github from '../../assets/github.svg'
+import linkedin from '../../assets/linkedin.svg'
 
 export default function Projects() {
   return (
   <footer>
     <div className="containerFooter">
-      <h2>CONTACT</h2>
-      <h1>
-          <span>Don't be shy! Hit me up! </span>
-         <img className='rotateImage' src="https://fonts.gstatic.com/s/e/notoemoji/latest/261d_fe0f/512.gif" alt="☝" width="32" height="32"/>
-      </h1>
+
  
        <div className='footerbottomRigth'>
-           <img src={mail} alt="" /> 
-             <p>bernard.pierrick0@gmail.com</p>
+          
+             <a href="mailto:bernard.pierrick0@gmail.com">
+             <img src={mail} alt="" />
+             </a>
+      
+             <a href="https://github.com/pierrick951">
+             <img src={github} alt="" />
+             </a>
+
+             <a href="https://www.linkedin.com/in/pierrick-bernard-etuve/">
+             <img src={linkedin} alt="" />
+             </a>
       
       </div>
     </div>
