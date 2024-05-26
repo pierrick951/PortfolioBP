8f907629d5f70f0b8ea8bd5c901f41b5c17a91e8
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-26 11:55:41 -0400

    modification du display pour les les elements de project box

--- PortfolioBP/src/components/Projects/MyProjects.jsx
+++ PortfolioBP/src/components/Projects/MyProjects.jsx
@@ -10,8 +10,9 @@ export default function MyProjects() {
         <div className="cardProject item2">t</div>
         <div className="cardProject item3">t</div>
         <div className="cardProject item4"></div>
-        <div className="cardProject item5"></div>
-        <div className="cardProject item6"></div>
+        <div className="cardProject item4"></div>
+        <div className="cardProject item4"></div>
+      
         </div>
       </div>
     </div>
