8420590eebc54bbb0ea49bdc6fabc57d8c24ec7c
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-26 19:19:43 -0400

    update card project

--- PortfolioBP/src/components/Projects/MyProjects.jsx
+++ PortfolioBP/src/components/Projects/MyProjects.jsx
@@ -1,4 +1,5 @@
 import './MyProjects.css'
+import swatch from '../../assets/swatch.png'
 export default function MyProjects() {
   return (
     <div className="containerProject">
@@ -6,7 +7,14 @@ export default function MyProjects() {
   
         <div className="projectBox">
           
-        <div className="cardProject item1">t</div>
+        <div className="cardProject item1">
+          <div className="topBox">
+            <img src={swatch} alt="" />
+          </div>
+          <div className='botBox'>
+
+          </div>
+        </div>
         <div className="cardProject item2">t</div>
         <div className="cardProject item3">t</div>
         <div className="cardProject item4"></div>
